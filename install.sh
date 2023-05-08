47b8bf292414ccfc722c500bb52eb160ac39e90d
Sławomir Paszko <folkus@gmail.com>
2023-05-08 23:59:31 +0200

    Updated.

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
+# Install all submodules repositories
 git submodule update --init --recursive
+
+# Additional Apps.
+# ----------------
+
+# Netron - visualization of networks.
+snap install netron
+
+
